479beb0f5b10873da75b87375ebaee4bf3038997
Sergey Zakharchenko <szakharchenko@open-sys.org>
2015-10-02 14:24:15 +0000

    Making GPIO2 float to work around IN1 boot-mode-related problems.

--- configure_gpio.lua
+++ configure_gpio.lua
@@ -1,7 +1,12 @@
 local p=require"pinout"
 
+local gpio_pullup_override ={
+    -- GPIO2 needs to float
+    [4]=gpio.FLOAT
+}
+
 for _,v in ipairs(p.input) do
-    gpio.mode(v,gpio.INPUT,gpio.PULLUP)
+    gpio.mode(v,gpio.INPUT,gpio_pullup_override[v] or gpio.PULLUP)
 end
 for _,v in ipairs(p.output) do
     gpio.mode(v,gpio.OUTPUT)
