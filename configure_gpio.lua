82431383b708e2123fa923c7afaaa0c7b66bfb02
Sergey Zakharchenko <szakharchenko@open-sys.org>
2015-10-02 18:50:19 +0000

    Fixing forcing GPIO2 to float.

--- configure_gpio.lua
+++ configure_gpio.lua
@@ -1,12 +1,18 @@
 local p=require"pinout"
 
-local gpio_pullup_override ={
+local gpio_floating ={
     -- GPIO2 needs to float
-    [4]=gpio.FLOAT
+    [4]=true
 }
 
 for _,v in ipairs(p.input) do
-    gpio.mode(v,gpio.INPUT,gpio_pullup_override[v] or gpio.PULLUP)
+    local mode
+    if gpio_floating[v] then
+        mode=gpio.FLOAT
+    else
+        mode=gpio.PULLUP
+    end
+    gpio.mode(v,gpio.INPUT,mode)
 end
 for _,v in ipairs(p.output) do
     gpio.mode(v,gpio.OUTPUT)
