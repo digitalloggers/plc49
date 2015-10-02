99fc295e717f17f148e9c19a1aadf38026f0534a
Sergey Zakharchenko <szakharchenko@open-sys.org>
2015-10-02 14:13:51 +0000

    Exposing version number to web UI.

--- web_ui_config_GET.lua
+++ web_ui_config_GET.lua
@@ -11,5 +11,5 @@ return function(state,s,data)
     -- XXX: Working around https://github.com/elua/elua/issues/69
     local ap_ssid,ap_password=wifi.ap.getconfig()
     local sta_ssid,sta_password=wifi.sta.getconfig()
-    return http_util.reply_json(s,{name=config.get_value("name"),wifi_mode=mode_map[wifi.getmode()],wifi_ap={ap_ssid,ap_password},wifi_sta={sta_ssid,sta_password}})
+    return http_util.reply_json(s,{name=config.get_value("name"),version=config.get_value("version"),wifi_mode=mode_map[wifi.getmode()],wifi_ap={ap_ssid,ap_password},wifi_sta={sta_ssid,sta_password}})
 end
