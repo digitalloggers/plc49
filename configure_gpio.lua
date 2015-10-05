442e197b386c1dbd7b4c5f498c3330bbbe601ef8
Sergey Zakharchenko <szakharchenko@open-sys.org>
2015-10-05 18:30:36 +0000

    Switching all inputs to floating as per new recommendations.

--- configure_gpio.lua
+++ configure_gpio.lua
@@ -1,18 +1,7 @@
 local p=require"pinout"
 
-local gpio_floating ={
-    -- GPIO2 needs to float
-    [4]=true
-}
-
 for _,v in ipairs(p.input) do
-    local mode
-    if gpio_floating[v] then
-        mode=gpio.FLOAT
-    else
-        mode=gpio.PULLUP
-    end
-    gpio.mode(v,gpio.INPUT,mode)
+    gpio.mode(v,gpio.INPUT,gpio.FLOAT)
 end
 for _,v in ipairs(p.output) do
     gpio.mode(v,gpio.OUTPUT)
